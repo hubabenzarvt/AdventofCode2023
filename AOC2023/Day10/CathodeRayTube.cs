88b1efd237bf3100236287e61a9e02ef20a44586
hubabenzarvt <h.benzar@vtime.net>
2023-09-25 17:03:05 +0100

    Update CathodeRayTube.cs

--- AOC2023/Day10/CathodeRayTube.cs
+++ AOC2023/Day10/CathodeRayTube.cs
@@ -27,8 +27,90 @@ public class CathodeRayTube
     ///
     ///
     /// </summary>
-    public void Part1()
+    
+    
+    private static string inputPath = File.OpenText("C:\\Users\\hbenzar\\Documents\\GitHub\\AdventofCode2023\\AOC2023\\Day10\\input.txt").ReadToEnd();
+    private static string[] lines = inputPath.Split(Environment.NewLine);
+    
+    private List<Tuple<string, int>> executeCommands = new List<Tuple<string, int>>();
+    private List<Tuple<string, int>> executingCommands = new List<Tuple<string, int>>();
+    private List<Tuple<string, int>> executedCommands = new List<Tuple<string, int>>();
+    
+    private int currentX = 0;
+    private int cycles = 0;
+    private int width = 39;
+    private int height = 0;
+    
+    private void ParseCommands()
+    {
+        foreach (var line in lines)
+        {
+            var split = line.Split(" ");
+            
+            var command = split[0];
+            if (split.Length == 1)
+            {
+                executeCommands.Add(new Tuple<string, int>(command, 0));
+                continue;
+            }
+            var amount = Int32.Parse(line.Split(" ")[1]);
+            if (command == "noop")
+                amount = 0;
+
+            executeCommands.Add(new Tuple<string, int>(command, amount));
+        }
+    }
+
+    private void CycleChecker(int tempX)
+    {
+        cycles++;
+        if (cycles == 20 || (cycles - 20) % 40 == 0)
+        {
+            currentX += tempX * cycles;
+        }
+    }
+
+    private void StartExecution()
+    {
+        int tempX = 1;
+
+        for (var i = 0; i < executeCommands.Count; i++)
+        {
+            CycleChecker(tempX);
+
+            if (executeCommands[i].Item1 != "noop")
+            {
+                executingCommands.Add(executeCommands[i]);
+                for (var j = 0; j < executingCommands.Count; j++)
+                {
+                    CycleChecker(tempX);
+                    tempX += executingCommands[j].Item2;
+                    executingCommands.RemoveAt(j);
+                }
+            }
+            executedCommands.Add(executeCommands[i]);
+        }
+    }
+
+    private void PlottingPoints()
     {
         
+    }
+    
+    public void Part1()
+    {
+        ParseCommands();
+        StartExecution();
+        
+        Console.WriteLine($"X Value {currentX}");
+        Console.WriteLine($"Cycles {cycles}");
+        Console.WriteLine($"Execute Commands {executeCommands.Count}");
+        Console.WriteLine($"Executing Commands {executingCommands.Count}");
+        Console.WriteLine($"Executed Commands {executedCommands.Count}");
+    }
+    
+    public void Part2()
+    {
+        Console.WriteLine($"Part 2 {currentX}");
     }
 }
